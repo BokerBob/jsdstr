f19591ff083ab7c0aa40ec2700612c4e36c0fd5a
tesss <andriybabets@gmail.com>
2013-09-02 00:07:13 +0300

    #8 js removed throw for test

--- JSDstr/JSDstr/Scripts/Script.js
+++ JSDstr/JSDstr/Scripts/Script.js
@@ -149,7 +149,6 @@
             var sum = 0;
             for (var i = 0; i <= 100000; i++) {
                 sum += i;
-                throw "kk";
             }
             return sum;
         };
